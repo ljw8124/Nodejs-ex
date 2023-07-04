7aaf44a4d0f40358e5afac5a32c303254046b36c
Joungwoo Lee <ljw8124@gmail.com>
2023-07-04 21:25:57 +0900

    promise 주석 및 promise.all() 예제

--- chapter04/style_promise.js
+++ chapter04/style_promise.js
@@ -1,7 +1,7 @@
 const DB = [];
 
 function saveDB(user) {
-  const oldDBSize = DB.length;
+  const oldDBSize = DB.length + 1;
   DB.push(user);
   console.log(`save ${user.name} to DB`);
 
@@ -31,7 +31,11 @@ function getResult(user) {
 
 function registerByPromise(user) {
   // 비동기 호출이지만 순서를 지켜서 실행됨
-  const result = saveDB(user).then(sendEmail).then(getResult);
+  const result = saveDB(user)
+      .then(sendEmail)
+      .then(getResult)
+      .catch(error => new Error(error))
+      .finally(() => console.log('process done!!'));
 
   // 이 단계에서는 아직 result 가 결과값을 가져오기도 전에 console.log 로 호출했기 때문에 'pending' 형태로 반환한다.
   console.log(result);
@@ -39,7 +43,7 @@ function registerByPromise(user) {
   return result;
 }
 
-const myUser = { email: 'ljw81242@naver.com', name: 'lee joungwoo2', age: '28 '}
+const myUser = { email: 'ljw8124@naver.com', name: 'lee joungwoo', age: '28'}
 
 const result = registerByPromise(myUser);
 
@@ -47,3 +51,15 @@ result.then(console.log);
 
 
 // 여기서 registerByPromise 가 Promise 객체를 반환하므로 then 으로 사용 가능하다.
+
+
+// ----------------------------------------------------------------------------------------------------
+
+// 동시에 여러 promise 호출하기
+//  const allResult = Promise.all([saveDB(myUser), sendEmail(myUser), getResult(myUser)]);
+//
+//  allResult.then(console.log);
+
+
+// JS 에서 promise 방식은 then() 과 catch() 를 이용하여 체이닝 방식으로 코딩을 해야하기 때문에 만만하지 않다
+// 거기에 더 복잡한 로직을 추가하고 예외처리 까지 해야하는 상황이 온다면 코딩하기가 더욱 힘들어진다.
