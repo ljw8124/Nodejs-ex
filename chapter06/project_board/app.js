ac4960f858e90efc6a2f82cb2c2a75b98d338fc5
Joungwoo Lee <ljw8124@gmail.com>
2023-07-06 21:28:12 +0900

    handlebars 연결

--- chapter06/project_board/app.js
+++ chapter06/project_board/app.js
@@ -6,7 +6,7 @@ const handlebars = require("express-handlebars");
 const app = express();
 
 // 템플릿 엔진으로 핸들바를 등록, 여기서 handlebars 는 파일의 확장자로 사용할 이름(다른걸로 변경해도됨)
-app.engine("handlebars", handlebars.engine());
+app.engine("handlebars", handlebars.engine()); // handlebars.engine({ layoutDir: "views" }) 와 같이 기본 레이아웃 디렉터리를 변경할 수도 있음
 
 // 웹페이지 로드시 사용할 템플릿 엔진을 핸들바로 설정, 위에서 설정한 이름 그대로 설정해주어야 함
 app.set("view engine", "handlebars");
@@ -15,7 +15,7 @@ app.set("view engine", "handlebars");
 app.set("views", __dirname+ "/views");
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "안녕하세요", message: "만나서 반갑습니다."});
+  res.render("home", { title: "안녕하세요", message: "만나서 반갑습니다."}); // 이 때 기본 레이아웃을 사용하고 싶지 않다면 결과 객체에 layout: false 를 추가하면 된다
 });
 
 app.listen(3000);
