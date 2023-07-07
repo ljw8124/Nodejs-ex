b1a9f1fa38d6ba9560261642b815f26314abb0f0
Joungwoo Lee <ljw8124@gmail.com>
2023-07-07 12:36:59 +0900

    필요한 view 모두 작성

--- chapter06/project_board/app.js
+++ chapter06/project_board/app.js
@@ -14,12 +14,21 @@ app.set("view engine", "handlebars");
 // 뷰 디렉터리를 해당 프로젝트 내 views 로 등록, 기본적으로는 상대 경로로 설정되는데 문제가 생길 수도 있으므로 절대경로로 변경
 app.set("views", __dirname+ "/views");
 
+// 메인페이지(홈)
 app.get("/", (req, res) => {
   res.render("home", { title: "테스트 게시판", message: "만나서 반갑습니다." }); // 이 때 기본 레이아웃을 사용하고 싶지 않다면 결과 객체에 layout: false 를 추가하면 된다
 });
 
+// 게시글 작성페이지
 app.get("/write", (req, res) => {
   res.render("write", { title: "테스트 게시판" });
+});
+
+// 게시글 조회페이지
+app.get("/detail/:id", (req, res) => {
+  res.render("detail", {
+    title: "테스트 게시판",
+  })  ;
 });
 
 app.listen(3000);
