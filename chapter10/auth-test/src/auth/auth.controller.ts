0eeda79a66c095672c3998358cc6af1d7db70c59
Joungwoo Lee <ljw8124@gmail.com>
2023-07-21 21:22:48 +0900

    회원가입 테스트

--- chapter10/auth-test/src/auth/auth.controller.ts
+++ chapter10/auth-test/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from "../user/user.dto";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    // AuthService 주입
+    constructor(private authService: AuthService) {}
+
+    @Post('register')
+    // class-validator 가 자동으로 유효성 검증
+    async getUser(@Body() userDto: CreateUserDto) {
+        return await this.authService.register(userDto);
+        // authService 를 사용해 user 정보 저장
+    }
+}
--- chapter10/auth-test/src/auth/auth.service.ts
+++ chapter10/auth-test/src/auth/auth.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from "../user/user.dto";
+import { UserService } from "../user/user.service";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    // 생성자에서 UserService 를 주입받음
+    constructor(private userService: UserService) {}
+
+    // 내부 메서드가 비동기 처리 하므로 async 로 선언
+    async register(userDto: CreateUserDto) {
+        // 이미 가입된 유저가 있는지 체크
+        const user = await this.userService.getUser(userDto.email);
+
+        if(user) {
+            throw new HttpException(
+                '해당 유저가 이미 존재합니다.',
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+
+        // 패스워드 암호화
+        const encrytedPassword = bcrypt.hashSync(userDto.password, 10);
+
+        // DB 에 저장, 저장 중 에러가 발생한다면 서버 에러 발생하도록 코딩
+        try {
+            const user = await this.userService.createUser({
+                ...userDto,
+                password: encrytedPassword,
+            });
+
+            // 회원 가입 후 반환하는 값에는 password 를 주지 않음
+            user.password = undefined;
+
+            return user;
+        } catch(err) {
+            throw new HttpException('서버 에러', 500);
+        }
+    }
+}
