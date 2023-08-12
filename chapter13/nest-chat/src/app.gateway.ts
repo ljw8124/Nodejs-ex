122da243622a4e7554fca7df510ab130f2240e4a
Joungwoo Lee <ljw8124@gmail.com>
2023-08-12 21:02:27 +0900

    nickname 설정

--- chapter13/nest-chat/src/app.gateway.ts
+++ chapter13/nest-chat/src/app.gateway.ts
@@ -12,8 +12,16 @@ export class ChatGateway {
     // message 이벤트 구독
     @SubscribeMessage('message')
     handleMessage(socket: Socket, data: any): void {
+
+        const {message, nickname} = data;
+
         // 접속한 클라이언트들에 메시지 전송
         // socket.io 에서는 모든 클라이언트 인스턴스에 임의의 id 값을 내려준다(20자)
-        this.server.emit('message', `client-${socket.id.substring(4, 5)} : ${data}`);
+        // this.server.emit('message', `client-${socket.id.substring(4, 5)} : ${data}`);
+
+        // broadcast.emit() 을 이용하면 전송을 요청한 클라이언트를 제외하고 다른 클라이언트들에게 데이터를 전송하므로
+        // 채팅을 할 때 내 메시지와 상대방 메시지를 구분하기 쉽다.
+        socket.broadcast.emit('message', `${nickname}: ${message}`);
     }
+
 }
--- chapter13/nest-chat/static/script.js
+++ chapter13/nest-chat/static/script.js
@@ -3,13 +3,24 @@
 // 네임스페이스는 복잡한 프로그램을 만들 시에 반드시 필요하다고 할 수 있다.
 const socket = io('http://localhost:3000/chat');
 
+// prompt 는 사용자에게 값을 받고 변수에 할당한다.
+const nickname = prompt('닉네임을 입력해주세요.');
+
 // console.log(socket);
 
 // 전송 버튼 클릭시 입력된 글을 message 로 보냄
 function sendMessage() {
   const message = document.querySelector('#message').value;
+  const chatDiv = document.querySelector('#chat');
 
-  socket.emit('message', message)
+  const divEle = document.createElement('div');
+  const messageNode = document.createTextNode(message);
+
+  divEle.append(messageNode);
+
+  chatDiv.append();
+
+  socket.emit('message', {message, nickname});
 }
 
 socket.on('connect', () => {
@@ -17,11 +28,11 @@ socket.on('connect', () => {
 });
 
 socket.on('message', (message) => {
-  const chatErea = document.querySelector('#chat');
+  const chatDiv = document.querySelector('#chat');
 
   const divEle = document.createElement('div');
   const messageNode = document.createTextNode(message);
 
   divEle.append(messageNode);
-  chatErea.appendChild(divEle);
+  chatDiv.appendChild(divEle);
 });
